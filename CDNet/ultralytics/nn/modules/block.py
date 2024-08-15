12719d53c0d2adbb0b39e920dfcafcf3bb7be1e5
htxhuo <75800822+htxhuo@users.noreply.github.com>
2024-08-16 00:14:59 +0800

    first commit

--- CDNet/ultralytics/nn/modules/block.py
+++ CDNet/ultralytics/nn/modules/block.py
@@ -13,125 +13,189 @@ __all__ = ('DFL', 'HGBlock', 'HGStem', 'SPP', 'SPPF', 'C1', 'C2', 'C3', 'C2f', '
            'GhostBottleneck', 'Bottleneck', 'BottleneckCSP', 'Proto', 'RepC3',
            'DAB')
 
-
 ############################################  DAB split attention  begin  ##############################################################
 
 
-# class h_sigmoid(nn.Module):
-#     def __init__(self, inplace=True):
-#         super(h_sigmoid, self).__init__()
-#         self.relu = nn.ReLU6(inplace=inplace)
+class h_sigmoid(nn.Module):
+    def __init__(self, inplace=True):
+        super(h_sigmoid, self).__init__()
+        self.relu = nn.ReLU6(inplace=inplace)
 
-#     def forward(self, x):
-#         return self.relu(x + 3) / 6
-
-
-# class h_swish(nn.Module):
-#     def __init__(self, inplace=True):
-#         super(h_swish, self).__init__()
-#         self.sigmoid = h_sigmoid(inplace=inplace)
-
-#     def forward(self, x):
-#         return x * self.sigmoid(x)
+    def forward(self, x):
+        return self.relu(x + 3) / 6
 
 
+class h_swish(nn.Module):
+    def __init__(self, inplace=True):
+        super(h_swish, self).__init__()
+        self.sigmoid = h_sigmoid(inplace=inplace)
 
-# class CoordAtt(nn.Module):
-#     def __init__(self, inp, reduction=32):
-#         super(CoordAtt, self).__init__()
-#         self.pool_h = nn.AdaptiveAvgPool2d((None, 1))
-#         self.pool_w = nn.AdaptiveAvgPool2d((1, None))
-
-#         mip = max(8, inp // reduction)
-
-#         self.conv1 = nn.Conv2d(inp, mip, kernel_size=1, stride=1, padding=0)
-#         self.bn1 = nn.BatchNorm2d(mip)
-#         self.act = h_swish()
-
-#         self.conv_h = nn.Conv2d(mip, inp, kernel_size=1, stride=1, padding=0)
-#         self.conv_w = nn.Conv2d(mip, inp, kernel_size=1, stride=1, padding=0)
-
-#     def forward(self, x):
-#         identity = x
-
-#         n, c, h, w = x.size()
-#         x_h = self.pool_h(x)
-#         x_w = self.pool_w(x).permute(0, 1, 3, 2)
-
-#         y = torch.cat([x_h, x_w], dim=2)
-#         y = self.conv1(y)
-#         y = self.bn1(y)
-#         y = self.act(y)
-
-#         x_h, x_w = torch.split(y, [h, w], dim=2)
-#         x_w = x_w.permute(0, 1, 3, 2)
-
-#         a_h = self.conv_h(x_h).sigmoid()
-#         a_w = self.conv_w(x_w).sigmoid()
-
-#         out = identity * a_w * a_h
-
-#         return out
-
-
-# def autopad(k, p=None, d=1):  # kernel, padding, dilation
-#     """Pad to 'same' shape outputs."""
-#     if d > 1:
-#         k = d * (k - 1) + 1 if isinstance(k, int) else [d * (x - 1) + 1 for x in k]  # actual kernel-size
-#     if p is None:
-#         p = k // 2 if isinstance(k, int) else [x // 2 for x in k]  # auto-pad
-#     return p
-
-
-# class Bottleneck_DAB(nn.Module):
-#     def __init__(self, c1, c2, shortcut=True, g=1, k=(3, 3), e=0.5):
-#         """Initializes a bottleneck module with given input/output channels, shortcut option, group, kernels, and
-#         expansion.
-#         """
-#         super().__init__()
-#         c_ = int(c2 * e)  # hidden channels
-#         self.cv1 = Conv(c1, c_, k[0], 1)
-#         self.cv2 = DCNv2(c_, c2, k[1], 1)
-#         self.add = shortcut and c1 == c2
-#         # self.attention = CoordAtt(c1)
-
-#     def forward(self, x):
-#         y = self.cv2(self.cv1(x))
-#         out = (x + y) if self.add else y
-#         return out
-
-# class DAB(nn.Module):
-#     def __init__(self, c1, c2, n=1, shortcut=False, g=1, e=0.5):
-#         """Initialize CSP bottleneck layer with two convolutions with arguments ch_in, ch_out, number, shortcut, groups,
-#         expansion.
-#         """
-#         super().__init__()
-#         self.c = int(c2 * e)  # hidden channels
-#         self.cv1 = Conv(c1, 2 * self.c, 1, 1)
-#         self.cv2 = Conv((2 + n) * self.c, c2, 1)  # optional act=FReLU(c2)
-#         self.attention = CoordAtt(c1)   
-#         # self.attention = CoordAtt(2 * self.c)   
-#         self.m = nn.ModuleList(Bottleneck_DAB(self.c, self.c, shortcut, g, k=((3, 3)), e=1.0) for _ in range(n))
-
-#     def forward(self, x):
-#         """Forward pass through C2f layer."""
-#         y = self.cv1(self.attention(x))
-#         # y = self.attention(self.cv1(x))
-#         y = list(y.chunk(2, 1))
-#         y.extend(m(y[-1]) for m in self.m)
-#         return self.cv2(torch.cat(y, 1))
-
-#     def forward_split(self, x):
-#         """Forward pass using split() instead of chunk()."""
-#         y = self.cv1(self.attention(x))
-#         # y = self.attention(self.cv1(x))
-#         y = list(y.split((self.c, self.c), 1))
-#         y.extend(m(y[-1]) for m in self.m)
-#         return self.cv2(torch.cat(y, 1))
+    def forward(self, x):
+        return x * self.sigmoid(x)
 
 
 
+class CoordAtt(nn.Module):
+    def __init__(self, inp, reduction=32):
+        super(CoordAtt, self).__init__()
+        self.pool_h = nn.AdaptiveAvgPool2d((None, 1))
+        self.pool_w = nn.AdaptiveAvgPool2d((1, None))
 
+        mip = max(8, inp // reduction)
+
+        self.conv1 = nn.Conv2d(inp, mip, kernel_size=1, stride=1, padding=0)
+        self.bn1 = nn.BatchNorm2d(mip)
+        self.act = h_swish()
+
+        self.conv_h = nn.Conv2d(mip, inp, kernel_size=1, stride=1, padding=0)
+        self.conv_w = nn.Conv2d(mip, inp, kernel_size=1, stride=1, padding=0)
+
+    def forward(self, x):
+        identity = x
+
+        n, c, h, w = x.size()
+        x_h = self.pool_h(x)
+        x_w = self.pool_w(x).permute(0, 1, 3, 2)
+
+        y = torch.cat([x_h, x_w], dim=2)
+        y = self.conv1(y)
+        y = self.bn1(y)
+        y = self.act(y)
+
+        x_h, x_w = torch.split(y, [h, w], dim=2)
+        x_w = x_w.permute(0, 1, 3, 2)
+
+        a_h = self.conv_h(x_h).sigmoid()
+        a_w = self.conv_w(x_w).sigmoid()
+
+        out = identity * a_w * a_h
+
+        return out
+
+
+def autopad(k, p=None, d=1):  # kernel, padding, dilation
+    """Pad to 'same' shape outputs."""
+    if d > 1:
+        k = d * (k - 1) + 1 if isinstance(k, int) else [d * (x - 1) + 1 for x in k]  # actual kernel-size
+    if p is None:
+        p = k // 2 if isinstance(k, int) else [x // 2 for x in k]  # auto-pad
+    return p
+
+
+class Bottleneck_DAB(nn.Module):
+    def __init__(self, c1, c2, shortcut=True, g=1, k=(3, 3), e=0.5):
+        """Initializes a bottleneck module with given input/output channels, shortcut option, group, kernels, and
+        expansion.
+        """
+        super().__init__()
+        c_ = int(c2 * e)  # hidden channels
+        self.cv1 = Conv(c1, c_, k[0], 1)
+        self.cv2 = DCN(c_, c2, k[1], 1)
+        self.add = shortcut and c1 == c2
+        # self.attention = CoordAtt(c1)
+
+    def forward(self, x):
+        y = self.cv2(self.cv1(x))
+        out = (x + y) if self.add else y
+        return out
+
+class DAB(nn.Module):
+    def __init__(self, c1, c2, n=1, shortcut=False, g=1, e=0.5):
+        """Initialize CSP bottleneck layer with two convolutions with arguments ch_in, ch_out, number, shortcut, groups,
+        expansion.
+        """
+        super().__init__()
+        self.c = int(c2 * e)  # hidden channels
+        self.cv1 = Conv(c1, 2 * self.c, 1, 1)
+        self.cv2 = Conv((2 + n) * self.c, c2, 1)  # optional act=FReLU(c2)
+        self.attention = CoordAtt(c1)   
+        # self.attention = CoordAtt(2 * self.c)   
+        self.m = nn.ModuleList(Bottleneck_DAB(self.c, self.c, shortcut, g, k=((3, 3)), e=1.0) for _ in range(n))
+
+    def forward(self, x):
+        """Forward pass through C2f layer."""
+        y = self.cv1(self.attention(x))
+        # y = self.attention(self.cv1(x))
+        y = list(y.chunk(2, 1))
+        y.extend(m(y[-1]) for m in self.m)
+        return self.cv2(torch.cat(y, 1))
+
+    def forward_split(self, x):
+        """Forward pass using split() instead of chunk()."""
+        y = self.cv1(self.attention(x))
+        # y = self.attention(self.cv1(x))
+        y = list(y.split((self.c, self.c), 1))
+        y.extend(m(y[-1]) for m in self.m)
+        return self.cv2(torch.cat(y, 1))
+
+
+
+
+class DCNv2(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, stride=1,
+                 padding=None, groups=1, dilation=1, act=True, deformable_groups=1):
+        super(DCNv2, self).__init__()
+
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.kernel_size = (kernel_size, kernel_size)
+        self.stride = (stride, stride)
+        padding = autopad(kernel_size, padding, dilation)
+        self.padding = (padding, padding)
+        self.dilation = (dilation, dilation)
+        self.groups = groups
+        self.deformable_groups = deformable_groups
+
+        self.weight = nn.Parameter(
+            torch.empty(out_channels, in_channels, *self.kernel_size)
+        )
+        self.bias = nn.Parameter(torch.empty(out_channels))
+
+        out_channels_offset_mask = (self.deformable_groups * 3 *
+                                    self.kernel_size[0] * self.kernel_size[1])
+        self.conv_offset_mask = nn.Conv2d(
+            self.in_channels,
+            out_channels_offset_mask,
+            kernel_size=self.kernel_size,
+            stride=self.stride,
+            padding=self.padding,
+            bias=True,
+        )
+        self.bn = nn.BatchNorm2d(out_channels)
+        self.act = Conv.default_act if act is True else act if isinstance(act, nn.Module) else nn.Identity()
+        self.reset_parameters()
+
+    def forward(self, x):
+        offset_mask = self.conv_offset_mask(x)
+        o1, o2, mask = torch.chunk(offset_mask, 3, dim=1)
+        offset = torch.cat((o1, o2), dim=1)
+        mask = torch.sigmoid(mask)
+        x = torch.ops.torchvision.deform_conv2d(
+            x,
+            self.weight,
+            offset,
+            mask,
+            self.bias,
+            self.stride[0], self.stride[1],
+            self.padding[0], self.padding[1],
+            self.dilation[0], self.dilation[1],
+            self.groups,
+            self.deformable_groups,
+            True
+        )
+        x = self.bn(x)
+        x = self.act(x)
+        return x
+
+    def reset_parameters(self):
+        n = self.in_channels
+        for k in self.kernel_size:
+            n *= k
+        std = 1. / math.sqrt(n)
+        self.weight.data.uniform_(-std, std)
+        self.bias.data.zero_()
+        self.conv_offset_mask.weight.data.zero_()
+        self.conv_offset_mask.bias.data.zero_()
 
 ############################################  DAB split attention  end    ##############################################################
 
